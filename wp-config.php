146688bce5fce372eb5a15c216f72f970245971a
Jip Moors <jhp.moors@gmail.com>
2017-04-19 21:49:44 +0200

    Initial commit

---
+++ wp-config.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Custom WordPress configurations on "wp-config.php" file.
+ *
+ * This file has the following configurations: MySQL settings, Table Prefix, Secret Keys, WordPress Language, ABSPATH and more.
+ * For more information visit {@link https://codex.wordpress.org/Editing_wp-config.php Editing wp-config.php} Codex page.
+ * Created using {@link http://generatewp.com/wp-config/ wp-config.php File Generator} on GenerateWP.com.
+ *
+ * @package WordPress
+ * @generator GenerateWP.com
+ */
+
+
+/* MySQL settings */
+define( 'DB_NAME',     'wordpress' );
+define( 'DB_USER',     'wordpress' );
+define( 'DB_PASSWORD', 'wordpress' );
+define( 'DB_HOST',     'db' );
+define( 'DB_CHARSET',  'utf8mb4' );
+
+
+/* Slightly modified to make sure code examples won't work */
+$table_prefix = 'wp__';
+
+
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_DISPLAY', true );
+
+
+/* Authentication Unique Keys and Salts. */
+/* https://api.wordpress.org/secret-key/1.1/salt/ */
+define( 'AUTH_KEY',         'put your unique phrase here' );
+define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
+define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
+define( 'NONCE_KEY',        'put your unique phrase here' );
+define( 'AUTH_SALT',        'put your unique phrase here' );
+define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
+define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
+define( 'NONCE_SALT',       'put your unique phrase here' );
+
+
+/* Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/* Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
